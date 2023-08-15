30e32a1776c9d2cb13ba5f7494f99b8f731f4e8c
jaidensy07 <jaidensy07@gmail.com>
2023-08-15 16:13:31 -0700

    updated views to return JSON

--- BudgetApp/views.py
+++ BudgetApp/views.py
@@ -1,49 +1,54 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponseBadRequest
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseNotFound
 from django.views import View
-from .models import Category, Item, UserIncome, UserCategoryBudget
+from .models import Item
 
-class CategoryListView(View):
-    def get(self, request):
-        categories = Category.objects.all()
-        return render(request, 'category_list.html', {'categories': categories})
+class ItemApiView(View):
+    def get(self, request, item_id=None):
+        if item_id is None:
+            items = Item.objects.all()
+            data = [{'id': item.id, 'name': item.name, 'price': item.price} for item in items]
+        else:
+            try:
+                item = Item.objects.get(id=item_id)
+                data = {'id': item.id, 'name': item.name, 'price': item.price}
+            except Item.DoesNotExist:
+                return HttpResponseNotFound()
 
-class ItemListView(View):
-    def get(self, request, category_id):
-        category = get_object_or_404(Category, pk=category_id)
-        items = Item.objects.filter(category=category)
-        return render(request, 'item_list.html', {'category': category, 'items': items})
+        return JsonResponse(data, safe=False)
 
-class ItemDetailView(View):
-    def get(self, request, item_id):
-        item = get_object_or_404(Item, pk=item_id)
-        return render(request, 'item_detail.html', {'item': item})
+    def post(self, request):
+        name = request.POST.get('name')
+        price = request.POST.get('price')
 
-    def post(self, request, item_id):
-        item = get_object_or_404(Item, pk=item_id)
-        item.name = request.POST['name']
-        item.price = request.POST['price']
+        if name and price:
+            item = Item.objects.create(name=name, price=price)
+            data = {'id': item.id, 'name': item.name, 'price': item.price}
+            return JsonResponse(data)
+        else:
+            return HttpResponseBadRequest()
+
+    def put(self, request, item_id):
+        try:
+            item = Item.objects.get(id=item_id)
+        except Item.DoesNotExist:
+            return HttpResponseNotFound()
+
+        name = request.POST.get('name', item.name)
+        price = request.POST.get('price', item.price)
+
+        item.name = name
+        item.price = price
         item.save()
-        return redirect('item_detail', item_id=item_id)
+
+        data = {'id': item.id, 'name': item.name, 'price': item.price}
+        return JsonResponse(data)
 
     def delete(self, request, item_id):
-        item = get_object_or_404(Item, pk=item_id)
+        try:
+            item = Item.objects.get(id=item_id)
+        except Item.DoesNotExist:
+            return HttpResponseNotFound()
+
         item.delete()
-        return redirect('category_list')
+        return JsonResponse({'message': 'Item deleted successfully'})
 
-class UserIncomeView(View):
-    def post(self, request):
-        income_amount = request.POST['income_amount']
-        user_income, created = UserIncome.objects.get_or_create(user=request.user)
-        user_income.income_amount = income_amount
-        user_income.save()
-        return redirect('category_list')
-
-class UserBudgetView(View):
-    def post(self, request, category_id):
-        category = get_object_or_404(Category, pk=category_id)
-        budget_amount = request.POST['budget_amount']
-        user_budget, created = UserCategoryBudget.objects.get_or_create(user=request.user, category=category)
-        user_budget.budget_amount = budget_amount
-        user_budget.save()
-        return redirect('category_list')
